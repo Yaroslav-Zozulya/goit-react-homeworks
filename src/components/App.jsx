b2a0ba191a35b7776890af1a6f8f28c099a44fbc
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-10-31 15:24:29 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,86 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import localStorageAPI from './localStorageAPI';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  checkContacts = name =>
+    this.state.contacts.find(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    );
+
+  componentDidMount() {
+    this.setState({
+      contacts: localStorageAPI.getContacts(),
+    });
+  }
+
+  componentDidUpdate() {
+    localStorageAPI.addContact(this.state.contacts);
+  }
+
+  addContact = (values, actions) => {
+    const { name, number } = values;
+    const { contacts } = this.state;
+
+    const isContact = this.checkContacts(name);
+    if (isContact) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    this.setState({
+      contacts: [
+        ...contacts,
+        {
+          id: nanoid(),
+          name,
+          number,
+        },
+      ],
+    });
+    actions.resetForm();
+  };
+
+  deleteContact = e => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        contact => contact.id !== e.target.id
+      ),
+    });
+  };
+
+  handleSearchInput = e => {
+    this.setState({ filter: e.target.value.toLowerCase().trim() });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const isContacts = contacts.length !== 0;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(filter)
+    );
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm values={this.state} addContact={this.addContact} />
+        {isContacts && (
+          <>
+            <h2>Contacts</h2>
+            <Filter handleSearchInput={this.handleSearchInput} />
+            <ContactsList
+              filteredContacts={filteredContacts}
+              deleteContact={this.deleteContact}
+            />
+          </>
+        )}
+      </>
+    );
+  }
+}
