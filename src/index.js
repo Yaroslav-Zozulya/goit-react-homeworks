b78a77985f3ec4b5a95c0ff9ba765bf153d5615b
Yaroslav Zozulya <91027713+Yaroslav-Zozulya@users.noreply.github.com>
2023-12-08 13:23:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-hw-08-phonebook">
+        <BrowserRouter basename="https://goit-react-homeworks-production.up.railway.app/">
           <App />
         </BrowserRouter>
       </PersistGate>
